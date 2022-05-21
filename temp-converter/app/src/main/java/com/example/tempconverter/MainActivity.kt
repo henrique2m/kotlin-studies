5a04c975eee5542af18506f02df601708c9cdcf4
henrique2m <btdavintehenrique@gmail.com>
2022-05-21 17:21:34 -0300

    chore: :sparkles: temp-converter: end  :rocket:

--- temp-converter/app/src/main/java/com/example/tempconverter/MainActivity.kt
+++ temp-converter/app/src/main/java/com/example/tempconverter/MainActivity.kt
@@ -2,10 +2,38 @@ package com.example.tempconverter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
 
 class MainActivity : AppCompatActivity() {
+    lateinit var valueTemp: EditText
+    lateinit var celsiusRadio: RadioButton
+    lateinit var fahrenheitRadio: RadioButton
+    lateinit var buttonConvertTemperature: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        valueTemp = findViewById<EditText>(R.id.valueTamp)
+        celsiusRadio = findViewById<RadioButton>(R.id.celsiusRadio)
+        fahrenheitRadio = findViewById<RadioButton>(R.id.fahrenheitRadio)
+        buttonConvertTemperature = findViewById<Button>(R.id.buttonConvertTemperature)
+
+        buttonConvertTemperature.setOnClickListener { converter(it) }
+    }
+
+    private fun converter(view: View) {
+        var temp: Double = valueTemp.text.toString().toDouble()
+
+        temp = if (celsiusRadio.isChecked) {
+            (temp - 32) * 5 / 9
+        } else {
+            temp * 9 / 5 + 32
+        }
+
+        valueTemp.setText(temp.toString())
     }
 }
